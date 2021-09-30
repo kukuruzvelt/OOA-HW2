03ec66a5c9827bf842321748a045b22a9c8c27b0
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2021-09-30 15:35:11 +0300

    Update main1.2.py

--- main1.2.py
+++ main1.2.py
@@ -1,16 +1,27 @@
 class Rational:
-    def __init__(self, num=1, denum=1): # add check if arguments are int and add simplification of numbers
+    def __init__(self, num=1, denom=1):
         self.__numerator = num
-        self.__denominator = denum
+        self.__denominator = denom
+        self.__simplification()
 
-    def __str__(self):
-        return str(self.__numerator) + "/" + str(self.__denominator)
+    def __simplification(self):
+        for i in range(2, 10):
+            temp1 = self.__numerator / i
+            temp2 = self.__denominator / i
+            if temp1.is_integer() and temp2.is_integer():
+                self.__numerator = int(temp1)
+                self.__denominator = int(temp2)
+                break
 
     def returnFraction(self):
-        print(self.__str__())
+        return str(self.__numerator) + "/" + str(self.__denominator)
 
     def printResult(self):
-        print(eval(self.__str__()))
+        return eval(self.returnFraction())
 
-r = Rational(2, 3)
-r.printResult()
+try:
+    r = Rational(5, 15)
+    print(r.returnFraction())
+    print(r.printResult())
+except TypeError as e:
+    print("ERROR")
